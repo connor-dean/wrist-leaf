48e6af30cb3f2a5223f7a9894b29b64924c90563
connor-dean <connor.timothy.dean@gmail.com>
2020-04-18 20:32:31 -0500

    Initial Commit

---
+++ wrist-leaf WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  wrist-leaf WatchKit Extension
+//
+//  Created by Connor Dean on 4/18/20.
+//  Copyright © 2020 Connor Dean. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ wrist-leaf WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  wrist-leaf WatchKit Extension
+//
+//  Created by Connor Dean on 4/18/20.
+//  Copyright © 2020 Connor Dean. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
